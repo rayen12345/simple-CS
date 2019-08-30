dfdbaa90dc3a4efbb309c2567ae241acae7ba796
rayen12345 <54586143+rayen12345@users.noreply.github.com>
2019-08-30 10:13:44 +0100

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calculator : MonoBehaviour
+{
+    void calculator(int a, int b)
+    {
+        int c = a + b;
+        int d = a - b;
+        int e = a * b;
+        int f = a / b;
+        int g = a % b;
+        Debug.Log(c + ", " + d + ", " + e + ", " + f +", " + g); 
+
+    }
+    void Start()
+    {
+        calculator(12, 5);
+            
+    }
+
+}
